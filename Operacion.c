ba03fec04af525e75da0e118ac2f512cc781a6f1
juanManuelGonzalezA00572003 <a00572003@tec.mx>
2022-09-14 18:39:02 -0500

    Corregir errores de mis compañeros

--- Operacion.c
+++ Operacion.c
@@ -1,15 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-<<<<<<< HEAD
-=======
-int suma(int a, int b){
- return a+b;
-}
-int resta(int a, int b){
- return a-b;
-}
->>>>>>> A01562528
+
 int main(){
 int num1 = 12;
 int num2 = 3;
